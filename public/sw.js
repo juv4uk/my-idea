0c4c82235edec825e538cd1ebe24d46075509928
juv4uk <85576956+juv4uk@users.noreply.github.com>
2026-08-06 11:26:06 +0300

    fix: make Service Worker caching resilient to individual resource failures

--- public/sw.js
+++ public/sw.js
@@ -16,7 +16,17 @@ const APP_SHELL = [
 // Кешуємо всю оболонку атомарно, щоб неповне встановлення не замінило робочу версію.
 // Die gesamte Hülle wird atomar gecacht, damit keine unvollständige Installation eine funktionierende Version ersetzt.
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return Promise.all(
+        APP_SHELL.map((url) => {
+          return cache.add(url).catch((err) => {
+            console.error('Failed to cache', url, err);
+          });
+        })
+      );
+    })
+  );
   self.skipWaiting();
 });
 
